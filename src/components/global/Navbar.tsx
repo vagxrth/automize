53a13c335e61074ccc08d28a9272ed133bad4d8a
Vagarth <vagarth419@gmail.com>
2024-11-03 06:45:39 +0530

    feat: modify logo

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -13,15 +13,15 @@ const Navbar = async (props: Props) => {
         <header className='fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between'>
             <aside className="flex items-center gap-[2px]">
                 <p className="text-3xl font-bold">
-                    First
+                    Auto
                 </p>
-                <Image src="/logo.png"
+                <Image src="/automize.png"
                     width={15}
                     height={15}
                     alt="logo"
                     className="shadow-sm" />
-                <p className="text-3xl font-bold">
-                    Million
+                    <p className="text-3xl font-bold">
+                    Mize
                 </p>
             </aside>
             <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
@@ -53,7 +53,7 @@ const Navbar = async (props: Props) => {
                 >
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                        {user ? 'Dashboard' : 'Connect Wallet'}
+                        {user ? 'Dashboard' : 'Get Started'}
                     </span>
                 </Link>
                 {user ? <UserButton afterSignOutUrl="/" /> : null}

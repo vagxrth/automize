24da9de3e636693f045395c484ffe20d19b22608
Vagarth <vagarth419@gmail.com>
2024-11-03 06:31:30 +0530

    feat: create custom workflow components

---
+++ src/app/(main)/(pages)/workflows/_components/Workflows.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Workflow from './Workflow'
+import { onGetWorkflows } from '../_actions/WorkflowConnections'
+import MoreCredits from './MoreCredits'
+
+type Props = {}
+
+const Workflows = async (props: Props) => {
+  const workflows = await onGetWorkflows()
+  return (
+    <div className="relative flex flex-col gap-4">
+      <section className="flex flex-col m-2">
+        <MoreCredits />
+        {workflows?.length ? (
+          workflows.map((flow: any) => (
+            <Workflow
+              key={flow.id}
+              {...flow}
+            />
+          ))
+        ) : (
+          <div className="mt-28 flex text-muted-foreground items-center justify-center">
+            No Workflows
+          </div>
+        )}
+      </section>
+    </div>
+  )
+}
+
+export default Workflows

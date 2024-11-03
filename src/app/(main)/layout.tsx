5460130a91fca0d34327474e1440857e075e6079
Vagarth <vagarth419@gmail.com>
2024-11-03 06:08:21 +0530

    create main page layout

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import InfoBar from '@/components/infobar/InfoBar'
+import Sidebar from '@/components/sidebar/Sidebar'
+import React from 'react'
+
+type Props = {children: React.ReactNode}
+
+const Layout = (props: Props) => {
+  return (
+    <div className='flex overflow-hidden h-screen'>
+        <Sidebar />
+        <div className='w-full'>
+            <InfoBar />
+            {props.children}
+        </div>
+    </div>
+  )
+}
+
+export default Layout
